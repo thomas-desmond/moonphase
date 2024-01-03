ee4f320c085960ed26f424ff54436cca19bee709
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-01-03 12:23:48 -0800

    Add middleware for redirecting based on geolocation

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  console.log(request.geo)
+  if (request.geo && request.geo.latitude && request.geo.longitude) {
+    return NextResponse.redirect(new URL(`/?lat=${request.geo.latitude}&lng=${request.geo.longitude}`))
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/',
+}
