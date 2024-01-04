ee0dbd0a78449e21a5bf9b462c1557da112b4168
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-01-03 19:06:10 -0800

    Fix

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   console.log(request.geo)
-  if (request.nextUrl.pathname.includes('/?')) {
+  if (request.url.includes('/?')) {
     return;
   }
 
